34f8258056e7cbf3a65b9b97b5a40f0238b2258a
vtching <valerie.tching@gmail.com>
2025-01-23 17:29:55 -0800

    Add client repo creation in bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,3 +21,7 @@ find ${PROJECT_NAME}-api -name "*.backup" -exec rm -f {} \;
 TIMESTAMP=$(date '+%Y%m%d%H%M%S')
 # Replace the migrate file to create user DB table
 mv ${PROJECT_NAME}-api/db/migrate/YYYYmmDDHHMMSS-create-user.js ${PROJECT_NAME}-api/db/migrate/${TIMESTAMP}-create-user.js
+
+gh repo create "${PROJECT_NAME}-api" --private --clone --template=vtching/react-native-expo-urql-nativewind-boilerplate
+find ${PROJECT_NAME}-client -type f -exec sed -i.backup s/myproject/${PROJECT_NAME}/g {} \;
+find ${PROJECT_NAME}-client -name "*.backup" -exec rm -f {} \;
